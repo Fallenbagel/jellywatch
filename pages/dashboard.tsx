2cf3de786c2af82ffbc567338d2f5303db2aee39
James Finch <LubricantJam@users.noreply.github.com>
2022-06-13 22:03:05 +0100

    Added Live TV Support.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -49,7 +49,7 @@ export default function DashboardPage({
   }
 
   return (
-    <div className="bg-gray-900 h-screen text-white">
+    <div className="bg-neutral-900 h-screen text-white">
       <div className="flex items-center justify-end p-4 h-20 bg-purple-900">
         <button
           onClick={() => {
@@ -70,12 +70,13 @@ export default function DashboardPage({
           }
           const userId = element.UserId;
           const url = Cookies.get("URL")
-          const itemId= element.NowPlayingItem.Type === "Movie" ? element.NowPlayingItem.Id : element.NowPlayingItem.SeriesId;
+          const itemId = element.NowPlayingItem.Type === "Episode" ? element.NowPlayingItem.SeriesId : element.NowPlayingItem.Id;
+          const isTvChannel = element.NowPlayingItem.Type === "TvChannel" ? true : false;
           const avatar = element.UserPrimaryImageTag ? `${url}/Users/${userId}/Images/Primary/?tag=${element.UserPrimaryImageTag}&quality=90` : "/user-icon-192x192.png";
           const stream = element.TranscodingInfo ? "Transcode" : "Direct Play"
           const audio = element.TranscodingInfo ? `${stream} (${(element.NowPlayingItem.MediaStreams.find(s => s.Type === "Audio"))?.Codec} -> ${element.TranscodingInfo.AudioCodec})` : `${stream} (${(element.NowPlayingItem.MediaStreams.find(s => s.Type === "Audio"))?.Codec})`
           const video = element.TranscodingInfo ? `${stream} (${(element.NowPlayingItem.MediaStreams.find(s => s.Type === "Video"))?.Codec} -> ${element.TranscodingInfo.VideoCodec})` : `${stream} (${(element.NowPlayingItem.MediaStreams.find(s => s.Type === "Video"))?.Codec})`
-          const playbackProgress = element.PlayState.PositionTicks ? element.PlayState.PositionTicks / element.NowPlayingItem.RunTimeTicks * 100 : 0
+          const playbackProgress = element.PlayState.PositionTicks && !isTvChannel ? element.PlayState.PositionTicks / element.NowPlayingItem.RunTimeTicks * 100 : 0
           const transcodingProgress = element.TranscodingInfo ? Math.floor(element.TranscodingInfo.CompletionPercentage) : -1
           const playState = element.PlayState.IsPaused ? "Paused" : "Playing"
           const bitrate_full = element.TranscodingInfo ? element.TranscodingInfo.Bitrate : (element.NowPlayingItem.MediaStreams.find(s => s.Type === "Video"))?.BitRate
@@ -110,7 +111,7 @@ export default function DashboardPage({
             deviceImage = "/JellyfinDesktop.svg"
             deviceColour = "bg-gradient-to-br from-[#AA5CC3] to-[#00A4DC]"
           }
-          /* element.DeviceName == "Chrome" ? product = "/chrome.svg" : product = "/JellyfinDesktop.svg"; */
+
           // check if bitrate is Kbps or Mbps if it is Kbps set it to Kbps if its Mbps set it to Mbps
           let bitrate = ""
           if (bitrate_full && bitrate_full > 1000000 ) bitrate = `${(bitrate_full / 1000000).toFixed(2)} Mbps`
@@ -123,7 +124,7 @@ export default function DashboardPage({
             backgroundSize: "cover",
           }
 
-          const moviePosterClass = "sm:w-52 overflow-hidden m-1.5"
+          const moviePosterClass = `hidden sm:block overflow-hidden m-1.5 ${isTvChannel ? 'h-32 my-auto' : 'sm:w-52'}`
           const moviePosterImage = `${url}/Items/${itemId}/Images/Primary?tag=${itemId}`
           const moviePosterTitle = element.NowPlayingItem.Name
 
@@ -144,16 +145,16 @@ export default function DashboardPage({
               }}
             >
               <div style={movieBackdropStyle}>
-                <div className="relative w-full max-w-full flex flex-col sm:flex-row backdrop-blur-sm bg-black/50 pb-5">
+                <div className="relative w-full max-w-full flex flex-col sm:flex-row backdrop-blur-sm bg-black/50 pb-5 min-h-[340px]">
                   <img className={moviePosterClass} src={moviePosterImage} alt={moviePosterTitle}/>
 
                   <div className="absolute left-90 right-0 m-1.5">
-                      <div className={`flex items-center justify-center w-16 h-16 ${deviceColour}`}>
+                      <div className={`flex items-center justify-center w-16 h-16 rounded ${deviceColour}`}>
                         <img className="w-12 h-12" src={deviceImage}/>
                       </div>
                   </div>
 
-                  <div className="p-2 flex flex-col gap-0.5">
+                  <div className="p-2 flex flex-col gap-0.5 ml-4 sm:ml-8">
                     <div className="flex gap-4">
                       <span className="text-xs text-white/50 uppercase text-right my-auto w-20">Product</span>
                       <span className="w-max">{element.Client}</span>
@@ -210,16 +211,20 @@ export default function DashboardPage({
 
                   {/* Display div on bottom on parent div */}
                   <div className="absolute bottom-0 left-0 min-w-full">
-                    <div className="w-full h-5 bg-gray-700 relative">
+                    <div className={`w-full h-5 relative ${isTvChannel ? 'bg-red-700' : 'bg-gray-700'}`}>
                       <div className="flex justify-between mx-1.5 text-sm font-medium text-white">
-                        <span className="z-20">{playState}</span>
-                        <span className="z-20">{Math.floor(playbackProgress)}%</span>
+                        <span className="z-20">{isTvChannel ? 'Live' : playState}</span>
+                        {!isTvChannel &&
+                          <span className="z-20">{Math.floor(playbackProgress)}%</span>
+                        }
                       </div>
 
-                      <div className="bg-blue-600 h-5 bottom-0 left-0 z-10 absolute" style={playbackProgressStyle}></div>
-                      {transcodingProgress > 0 &&
-                        <div className="bg-orange-600 h-5 absolute bottom-0 left-0 z-0" style={transcodingProgressStyle}></div>
-                      }
+                      {/* Display progress bar */}
+                        <div className="bg-blue-600 h-5 bottom-0 left-0 z-10 absolute" style={playbackProgressStyle}></div>
+
+                        {transcodingProgress > 0 &&
+                          <div className="bg-orange-600 h-5 absolute bottom-0 left-0 z-0" style={transcodingProgressStyle}></div>
+                        }
                     </div>
                   </div>
                 </div>
